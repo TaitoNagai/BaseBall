24ac8d26b26eddd36380caf32dc6647088aa8c5a
DESKTOP-L38QT5L\vantan <tai-susi@ezweb.ne.jp>
2021-09-22 19:17:59 +0900

    a

---
+++ Assets/Script/BallKinds.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class BallKinds : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] UnityEvent ballKind;
+    [SerializeField] UnityEvent ballKindSlow;
+    [SerializeField] UnityEvent ballKindDown;
+
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            ballKind.Invoke();
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            ballKindSlow.Invoke();
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            ballKindDown.Invoke();
+        }
+    }
+}
---
+++ Assets/Script/ballH.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballH : MonoBehaviour
+{
+    [SerializeField] GameObject ballPos;
+    [SerializeField] GameObject ball;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(ball, ballPos.transform.position, Quaternion.identity);
+        }
+    }
+}
