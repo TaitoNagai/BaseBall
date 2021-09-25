957d4ab0096fe9e1f70de812c7754f9385af4f3d
DESKTOP-L38QT5L\vantan <tai-susi@ezweb.ne.jp>
2021-09-25 17:17:10 +0900

    9/25 17:17

--- Assets/Script/BallDestroy.cs
+++ Assets/Script/BallDestroy.cs
@@ -7,7 +7,7 @@ public class BallDestroy : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "End")
+        if (collision.gameObject.tag == "Strike")
         {
             Destroy(collision.gameObject);
         }
--- Assets/Script/CountPanel2.cs
+++ Assets/Script/CountPanel2.cs
@@ -2,51 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CountPanel : MonoBehaviour
+public class CountPanel2 : MonoBehaviour
 {
-    // Start is called before the first frame update
-    [SerializeField] GameObject m_strikeBallImage;
-    [SerializeField] GameObject m_strikeBallImage2;
-    int StrikeCount = 0;
     [SerializeField] GameObject m_BallImage;
     [SerializeField] GameObject m_BallImage2;
     [SerializeField] GameObject m_BallImage3;
     int BallCount = 0;
 
+    // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        StrikeBall();
         BallBall();
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (this.gameObject.tag == "Strike")
+        if (collision.gameObject.tag == "Strike")
         {
-            ++StrikeCount;
             ++BallCount;
-        }
-    }
-    void StrikeBall()
-    {
-        if (StrikeCount == 1)
-        {
-            m_strikeBallImage.SetActive(true);
-        }
-        else if (StrikeCount == 2)
-        {
-            m_strikeBallImage2.SetActive(true);
-        }
-        else if (StrikeCount == 3)
-        {
-            StrikeCount = 0;
-            m_strikeBallImage.SetActive(false);
-            m_strikeBallImage2.SetActive(false);
         }
     }
     void BallBall()
